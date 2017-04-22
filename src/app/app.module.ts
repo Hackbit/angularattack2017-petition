0d3143ffe6f8614f79e6b7e66d5806816659ddfb
Yaumel9 <ybeta9@gmail.com>
2017-04-21 22:56:32 -0400

    Adds the petition form creator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { FormCreatorComponent } from './components/petitionformcreator/petitionform'
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, FormCreatorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/petitionformcreator/petitionform.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'form-creator-component',
+  templateUrl: `petitionform.html`,
+})
+export class FormCreatorComponent  { }
