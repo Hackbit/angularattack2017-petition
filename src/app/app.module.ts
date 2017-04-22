1dbe248a183bd180380e370d2f4a68395360f1a1
esolar07 <esolar07@gmail.com>
2017-04-22 01:20:18 -0400

    added petition background component. Also added css styles to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { FormCreatorComponent } from './components/petitionformcreator/petitionform'
+import { FormCreatorComponent } from './components/petitionformcreator/petitionform';
+import { PetitionBackgroundComponent } from './components/petitionbackgroundlist/petitionbackground';
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, FormCreatorComponent ],
+  declarations: [ AppComponent, FormCreatorComponent, PetitionBackgroundComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
