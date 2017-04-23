a26d949e9ed63c8bb765caaf773edc0d843d71ca
= <rafael@nerd.com>
2017-04-22 20:37:44 -0400

    about page changes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'about',
-  templateUrl: `about.component.html`,
+    moduleId: module.id,
+    selector: 'about',
+    templateUrl: 'about.component.html',
 })
-export class AboutComponent  { }
+
+export class AboutComponent  { 
+    devs: any;
+
+    constructor() {
+        this.devs = [
+            {
+                name: 'Eddie',
+                phone: '305-555-1111',
+                email: 'name1@email1.com'
+            },
+            {
+                name: 'Yaumel',
+                phone: '305-555-2222',
+                email: 'name2@email.com'
+            },
+            {
+                name: 'The other guy',
+                phone: '305-555-3333',
+                email: 'name3@email.com'
+            },           
+        ]
+    }
+}
